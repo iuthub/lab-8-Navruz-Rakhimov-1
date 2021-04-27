550b6c54d214855051c8652bbd3a8536d9cb6b12
navruz <rakhimovnavruzbek@gmail.com>
2021-04-27 16:27:51 +0500

    lab 8

--- resources/views/blog/post.blade.php
+++ resources/views/blog/post.blade.php
@@ -3,11 +3,8 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <p class="quote">Learning Laravel</p>
+            <p class="quote">{{ $post['title'] }}</p>
         </div>
     </div>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aperiam corporis id magnam maiores necessitatibus odit perferendis quod ratione reiciendis sequi sunt tempore, veniam veritatis vero. Ipsum laboriosam quaerat vitae.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, ratione tempore. Assumenda at culpa eius est facilis nam quam, rem. Aliquid dolorem dolorum error ex iste officiis similique. Fugit, sapiente.
-    </p>
+    <p>{{ $post['content'] }}</p>
 @endsection
--- resources/views/partials/admin-header.blade.php
+++ resources/views/partials/admin-header.blade.php
@@ -3,7 +3,7 @@
         <div class="navbar-header">
             <a href="#" class="navbar-brand">Laravel Guide</a>
             <ul class="nav navbar-nav">
-                <li><a href="{{ route('admin.index') }}">Posts</a></li>
+                <li><a href="{{ route('blog.index') }}">Posts</a></li>
             </ul>
         </div>
     </div>
